05e9812247187d96b35ffc4d7743dfa10776fa76
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2023-08-30 18:19:12 +0500

    Prepared the markup

--- index.php
+++ index.php
@@ -3,8 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Drag&Drop</title>
+    <!--    <link rel="stylesheet" href="src/css/style.css">-->
 </head>
 <body>
-    
+<div class="drag-and-drop ">
+    <h3 class="drag-and-drop-title">
+        Перетащите файл для загрузки
+    </h3>
+</div>
+
+<script src="src/js/app.js"></script>
 </body>
 </html>
